736c0c8ce86f177d3c6b202de7fb682810006360
Gavinmr1 <gavin_riggs@science.edu>
2024-08-29 11:14:01 -0400

    first push to repo

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{js,ts,jsx,tsx}'],
+  darkMode: ['class'],
+  theme: {
+    extend: {
+      spacing: {
+        spacing: 'var(--spacing)',
+        'inner-spacing': 'var(--inner-spacing)',
+      },
+      colors: {
+        background: 'var(--background)',
+        foreground: 'var(--foreground)',
+        primary: 'var(--primary)',
+        secondary: 'var(--secondary)',
+      },
+    },
+  },
+  plugins: [],
+}
